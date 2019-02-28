cea8b9b1f0608b1f17831ced98c38c1654ffad3d
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-02-28 16:04:45 -0500

    Refactored back to as I was starting yesterday to manage UI state in the form component it's self.  got add functionality back.

--- friends/src/components/FriendForm/FriendForm.js
+++ friends/src/components/FriendForm/FriendForm.js
@@ -5,13 +5,38 @@ import styles from "./FriendForm.module.scss";
 class FriendForm extends React.Component {
   constructor() {
     super();
-    //this.state = {
-    //name: "",
-    //age: "",
-    //email: "",
-    //id: ""
-    //};
+    this.state = {
+      friend: {
+        name: "",
+        age: "",
+        email: "",
+        id: ""
+      }
+    };
   }
+
+  changeHandler = e => {
+    e.persist();
+    this.setState(prevState => ({
+      friend: {
+        ...prevState.friend,
+        [e.target.name]: e.target.value
+      }
+    }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addFriend(e, this.state.friend);
+    this.setState({
+      friend: {
+        name: "",
+        age: "",
+        email: "",
+        id: ""
+      }
+    });
+  };
 
   //changeHandler = e => {
   //this.setState({ [e.target.name]: e.target.value });
@@ -20,36 +45,42 @@ class FriendForm extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <form className={styles.Form} onSubmit={e => this.props.addFriend(e)}>
+        <form className={styles.Form} onSubmit={this.handleSubmit}>
           <input
             type="text"
             placeholder="Friend's Name"
             name="name"
-            value={this.props.name}
+            value={this.state.friend.name}
+            //value={this.props.name}
             required
-            onChange={e =>
-              this.props.changeHandler({ [e.target.name]: e.target.value })
-            }
+            onChange={this.changeHandler}
+            //onChange={e =>
+            //this.props.changeHandler({ [e.target.name]: e.target.value })
+            //}
           />
           <input
             type="text"
             placeholder="Friend's Age"
             name="age"
-            value={this.props.age}
+            value={this.state.friend.age}
+            //value={this.props.age}
             required
-            onChange={e =>
-              this.props.changeHandler({ [e.target.name]: e.target.value })
-            }
+            onChange={this.changeHandler}
+            //onChange={e =>
+            //this.props.changeHandler({ [e.target.name]: e.target.value })
+            //}
           />
           <input
             type="text"
             placeholder="Friend's Email"
             name="email"
-            value={this.props.email}
+            value={this.state.friend.email}
+            //value={this.props.email}
             required
-            onChange={e =>
-              this.props.changeHandler({ [e.target.name]: e.target.value })
-            }
+            onChange={this.changeHandler}
+            //onChange={e =>
+            //this.props.changeHandler({ [e.target.name]: e.target.value })
+            //}
           />
           <button>Add Friend</button>
         </form>
--- friends/src/components/FriendsList/FriendsList.js
+++ friends/src/components/FriendsList/FriendsList.js
@@ -11,12 +11,13 @@ class FriendsList extends React.Component {
   constructor() {
     super();
     this.state = {
+      activeFriend: null,
       friends: [],
-      error: "",
-      name: "",
-      age: "",
-      email: "",
-      id: ""
+      error: ""
+      //name: "",
+      //age: "",
+      //email: "",
+      //id: ""
     };
   }
 
@@ -33,38 +34,39 @@ class FriendsList extends React.Component {
       });
   }
 
-  changeHandler = property => {
-    this.setState(property);
-  };
-
-  addFriend = e => {
-    //debugger;
+  addFriend = (e, newFriend) => {
     e.preventDefault();
+    console.log("test");
     axios
-      .post("http://localhost:5000/friends", {
-        name: this.state.name,
-        age: this.state.age,
-        email: this.state.email
-      })
+      .post("http://localhost:5000/friends", newFriend)
       .then(res => {
-        this.setState({ friends: res.data, name: "", age: "", email: "" });
+        console.log(res);
+        this.setState({
+          friends: res.data
+        });
       })
-      .catch(err => console.log("2nd error"));
-    //this.setState({ name: "", age:"", email: "" });
-
-    //this.setState({
-    //friends: [
-    //...this.state.friends,
-    //...this.state.error,
-    //this.state.name,
-    //this.state.age,
-    //this.state.email
-    //],
-    //name: "",
-    //age: "",
-    //email: ""
-    //});
+      .catch(err => console.log("error...", err));
   };
+
+  //changeHandler = property => {
+  //this.setState(property);
+  //};
+
+  //addFriend = e => {
+  ////debugger;
+  //e.preventDefault();
+  //axios
+  //.post("http://localhost:5000/friends", {
+  //name: this.state.name,
+  //age: this.state.age,
+  //email: this.state.email
+  //})
+  //.then(res => {
+  //this.setState({ friends: res.data, name: "", age: "", email: "" });
+  //})
+  //.catch(err => console.log("2nd error"));
+
+  //this.setState({ name: "", age:"", email: "" });
 
   render() {
     return (
@@ -72,10 +74,11 @@ class FriendsList extends React.Component {
         <h1 className={styles.Heading}>List of Friends!</h1>
         <FriendForm
           addFriend={this.addFriend}
-          name={this.state.name}
-          age={this.state.age}
-          email={this.state.email}
-          changeHandler={this.changeHandler}
+          //addFriend={this.addFriend}
+          //name={this.state.name}
+          //age={this.state.age}
+          //email={this.state.email}
+          //changeHandler={this.changeHandler}
         />
         {this.state.friends.map(friend => {
           return <Friend key={friend.id} friend={friend} />;
